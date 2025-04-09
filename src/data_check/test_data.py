55acd664f02bebdd153957131f3874628d7232b3
Paul Fox <paperbowser13@gmail.com>
2025-04-08 19:26:15 -0500

    added test_row_count and test_price_range to src/data_check/test_data.py

--- src/data_check/test_data.py
+++ src/data_check/test_data.py
@@ -63,3 +63,15 @@ def test_similar_neigh_distrib(data: pd.DataFrame, ref_data: pd.DataFrame, kl_th
 ########################################################
 # Implement here test_row_count and test_price_range   #
 ########################################################
+def test_row_count(data: pd.DataFrame):
+    """
+    Ensures the size of the dataset is reasonable (not too small, not too large)
+    """
+    assert 15000 < data.shape[0] < 1000000
+
+
+def test_price_range(data: pd.DataFrame, min_price: int | float, max_price: int | float):
+    """
+    Ensures the price range is between the defined minimum and maximum price values
+    """
+    assert data['price'].between(min_price, max_price)
